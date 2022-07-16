6b58ec73c2129fc50b68d6788a2b2a87d238b403
hsamani <123456@aA>
2022-07-16 16:26:19 +0430

    Card and Btn component added

--- src/components/User/Add.js
+++ src/components/User/Add.js
@@ -1,3 +1,6 @@
+import Card from '../UI/Card'
+import Btn from '../UI/Btn'
+
 const AddUser = () => {
 
     const onSubmit = (e) => {
@@ -5,24 +8,27 @@ const AddUser = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <div className="flex items-center py-2">
-                    <label htmlFor="name" className="min-w-10">Name</label>
-                    <input id="name" type="text" className="flex-1 px-2 py-1 ml-2 outline-none border rounded-lg " />
-                </div>
+        <Card>
+            <div>
+                <form onSubmit={onSubmit}>
+                    <div className="flex items-center py-2">
+                        <label htmlFor="name" className="font-bold text-sm">Name: </label>
+                        <input id="name" type="text" className="flex-1 px-2 py-1 ml-2 outline-none border rounded-lg " />
+                    </div>
 
-                <div className="flex items-center py-2">
-                    <label htmlFor="age" className="min-w-10">Age</label>
-                    <input id="age" type="number" className="flex-1 px-2 py-1 ml-2 outline-none border rounded-lg " />
-                </div>
-                <div>
-                    <button type="submit" className="py-2 px-6">
-                        Add user
-                    </button>
-                </div>
-            </form>
-        </div>
+                    <div className="flex items-center py-2">
+                        <label htmlFor="age" className="font-bold text-sm">Age: </label>
+                        <input id="age" type="number" className="flex-1 px-2 py-1 ml-2 outline-none border rounded-lg " />
+                    </div>
+                    <hr className='mt-4 mb-1'></hr>
+                    <div>
+                        <Btn type="submit" className="py-2 px-6">
+                            Add user
+                        </Btn>
+                    </div>
+                </form>
+            </div>
+        </Card>
     )
 }
 
