667053d1a373b0eb79988cb0a850f8f4998e5397
Hossein Samani <samanihossein1379@gmail.com>
2022-07-18 22:38:25 +0430

    add user component fixed

--- src/components/User/Add.js
+++ src/components/User/Add.js
@@ -1,51 +1,69 @@
-import { useState } from 'react'
-import Card from '../UI/Card'
-import Btn from '../UI/Btn'
+import { useState } from "react";
 
-const AddUser = () => {
-    const [name, setName] = useState('')
-    const [age, setAge] = useState('')
+import Card from "../UI/Card";
+import Btn from "../UI/Btn";
 
-    const nameHandler = (e) => {
-        setName(e)
-    }
+const AddUser = ({ onAddUser }) => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
-    const ageHandler = (e) => {
-        setAge(e)
-    }
+  const nameHandler = (e) => {
+    setName(e);
+  };
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-        if (name.trim().length === 0 || age.trim().length === 0) return
-        if (+age < 1) return
-        setName('')
-        setAge('')
-    }
+  const ageHandler = (e) => {
+    setAge(e);
+  };
 
-    return (
-        <Card>
-            <div>
-                <form onSubmit={onSubmit}>
-                    <div className="flex items-center py-2">
-                        <label htmlFor="name" className="font-bold text-sm">Name: </label>
-                        <input value={name} onChange={(e) => nameHandler(e.target.value)} id="name" type="text" className="flex-1 px-2 py-1 ml-2 outline-none border rounded-lg " />
-                    </div>
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (name.trim().length === 0 || age.trim().length === 0) return;
+    if (+age < 1) return;
+    onAddUser({ name, age });
+    setName("");
+    setAge("");
+  };
 
-                    <div className="flex items-center py-2">
-                        <label htmlFor="age" className="font-bold text-sm">Age: </label>
-                        <input value={age} onChange={(e) => ageHandler(e.target.value)} id="age" type="number" className="flex-1 px-2 py-1 ml-2 outline-none border rounded-lg " />
-                    </div>
-                    <hr className='mt-4 mb-1'></hr>
+  return (
+    <Card>
+      <div>
+        <form onSubmit={onSubmit}>
+          <div className="flex items-center py-2">
+            <label htmlFor="name" className="font-bold text-sm">
+              Name:{" "}
+            </label>
+            <input
+              value={name}
+              onChange={(e) => nameHandler(e.target.value)}
+              id="name"
+              type="text"
+              className="flex-1 px-2 py-1 ml-2 outline-none border rounded-lg "
+            />
+          </div>
 
-                    <div>
-                        <Btn type="submit" className="py-2 px-6">
-                            Add user
-                        </Btn>
-                    </div>
-                </form>
-            </div>
-        </Card>
-    )
-}
+          <div className="flex items-center py-2">
+            <label htmlFor="age" className="font-bold text-sm">
+              Age:{" "}
+            </label>
+            <input
+              value={age}
+              onChange={(e) => ageHandler(e.target.value)}
+              id="age"
+              type="number"
+              className="flex-1 px-2 py-1 ml-2 outline-none border rounded-lg "
+            />
+          </div>
+          <hr className="mt-4 mb-1"></hr>
 
-export default AddUser
+          <div>
+            <Btn type="submit" className="py-2 px-6">
+              Add user
+            </Btn>
+          </div>
+        </form>
+      </div>
+    </Card>
+  );
+};
+
+export default AddUser;
