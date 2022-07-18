cb362a4f4c87f683e24911e06b2b5f1d3415c7e5
Hossein Samani <samanihossein1379@gmail.com>
2022-07-18 22:39:59 +0430

    onAddUserHandler func added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import AddUser from './components/User/Add'
-import './App.css';
+import { useState } from "react";
+import AddUser from "./components/User/Add";
+import UserList from "./components/User/List";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const onAddUserHandler = (user) => {
+    setUsers((pervState) => [...pervState, user]);
+  };
+
   return (
     <div className="App w-1/2 mx-auto">
-      <AddUser />
+      <AddUser onAddUser={onAddUserHandler} />
+      <UserList users={users} />
     </div>
   );
 }
